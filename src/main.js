7e2a44e92a820963193a8586bbafc6aa625be5dc
FailedMaster <failedmaster666@gmail.com>
2026-07-29 10:09:38 +0200

    Clean out placeholder page

--- src/main.js
+++ src/main.js
@@ -2,17 +2,3 @@ import "./style.css";
 
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
-
-$("#app").innerHTML = `
-  <div>
-    <h1>Hello Finkenroulette!</h1>
-    <img src="/mysterybeer.png"/ style="width: 300px; height: 300px;"/>
-  </div>
-`;
-
-$("img").addEventListener("click", () => {
-  $("#app").insertAdjacentHTML(
-    "beforeend",
-    '<span style="font-size: 8em;">🍺</span>',
-  );
-});
